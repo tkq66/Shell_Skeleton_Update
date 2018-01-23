79a4f95dceaceaf599f18626f6edaaeb09eabc2a
TK <teekayu8@hotmail.com>
2018-01-23 14:46:26 +0800

    Update grep app interface

--- src/sg/edu/nus/comp/cs4218/app/Grep.java
+++ src/sg/edu/nus/comp/cs4218/app/Grep.java
@@ -1,5 +1,7 @@
 package sg.edu.nus.comp.cs4218.app;
 
+import java.io.InputStream;
+
 import sg.edu.nus.comp.cs4218.Application;
 
 public interface Grep extends Application {
@@ -7,34 +9,48 @@ public interface Grep extends Application {
 	/**
 	 * Returns string containing lines which match the specified pattern in
 	 * Stdin
-	 * @param args String containing command and arguments
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param stdin InputStream containing arguments from Stdin
 	 */
-	public String grepFromStdin(String args);
+	public String grepFromStdin(String pattern, InputStream stdin);
 
 	/**
 	 * Returns string containing lines which match the specified pattern in the
 	 * given file
-	 * @param args  String containing command and arguments
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param fileName String specifying name of the file
 	 */
-	public String grepFromOneFile(String args);
+	public String grepFromOneFile(String pattern, String fileName);
 
 	/**
 	 * Returns string containing lines which match the specified pattern in the
 	 * given files
-	 * @param args  String containing command and arguments
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param fileNames Array of file names
 	 */
-	public String grepFromMultipleFiles(String args);
-
-	/**
-	 * Returns string when invalid pattern is specified in grep from Stdin
-	 * @param args String containing command and arguments
-	 */
-	public String grepInvalidPatternInStdin(String args);
-
+	public String grepFromMultipleFiles(String pattern, String[] fileNames);
 	
 	/**
-	 * Returns string when invalid pattern is specified in grep from file
-	 * @param args String containing command and arguments
+	 * Returns string that doesn't contain lines which match the specified pattern 
+	 * in Stdin
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param stdin InputStream containing arguments from Stdin
 	 */
-	public String grepInvalidPatternInFile(String args);
+	public String grepInvertFromStdin(String pattern, InputStream stdin);
+
+	/**
+	 * Returns string that doesn't contain lines which match the specified pattern
+	 * given file
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param fileName String specifying name of the file
+	 */
+	public String grepInvertFromOneFile(String pattern, String fileName);
+
+	/**
+	 * Returns string that doesn't contain lines which match the specified pattern 
+	 * in the given files
+	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param fileNames Array of file names
+	 */
+	public String grepInvertFromMultipleFiles(String pattern, String[] fileNames);
 }
