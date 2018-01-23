e8f87037a5dd2fdfcd1e30b9f01ae604724c2ec2
TK <teekayu8@hotmail.com>
2018-01-23 18:53:33 +0800

    Add exit app interface:

---
+++ src/sg/edu/nus/comp/cs4218/app/Exit.java
@@ -0,0 +1,12 @@
+package sg.edu.nus.comp.cs4218.app;
+
+import sg.edu.nus.comp.cs4218.Application;
+
+public interface Exit extends Application {
+
+	/**
+	 * Terminate shell.
+	 */
+	public void terminateExecution();
+	
+}
