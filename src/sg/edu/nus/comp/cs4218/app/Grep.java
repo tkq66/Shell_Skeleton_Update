981138e4f949a957e44bbe78e727ee514c9f0488
TK <teekayu8@hotmail.com>
2018-01-29 19:00:09 +0800

    make Grep more elegant

--- src/sg/edu/nus/comp/cs4218/app/Grep.java
+++ src/sg/edu/nus/comp/cs4218/app/Grep.java
@@ -10,47 +10,22 @@ public interface Grep extends Application {
 	 * Returns string containing lines which match the specified pattern in
 	 * Stdin
 	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param isInvert Boolean option to filter the output that doesn't match 
+	 * the pattern
 	 * @param stdin InputStream containing arguments from Stdin
 	 */
-	public String grepFromStdin(String pattern, InputStream stdin);
-
-	/**
-	 * Returns string containing lines which match the specified pattern in the
-	 * given file
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param fileName String specifying name of the file
-	 */
-	public String grepFromOneFile(String pattern, String fileName);
+	public String grepFromStdin(String pattern, Boolean isInvert, InputStream stdin) 
+			throws Exception;
 
 	/**
 	 * Returns string containing lines which match the specified pattern in the
 	 * given files
 	 * @param pattern String specifying a regular expression in JAVA format
+	 * @param isInvert Boolean option to filter the output that doesn't match 
+	 * the pattern
 	 * @param fileNames Array of file names
 	 */
-	public String grepFromMultipleFiles(String pattern, String[] fileNames);
+	public String grepFromMultipleFiles(String pattern, Boolean isInvert, String... fileNames) 
+			throws Exception;
 	
-	/**
-	 * Returns string that doesn't contain lines which match the specified pattern 
-	 * in Stdin
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param stdin InputStream containing arguments from Stdin
-	 */
-	public String grepInvertFromStdin(String pattern, InputStream stdin);
-
-	/**
-	 * Returns string that doesn't contain lines which match the specified pattern
-	 * given file
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param fileName String specifying name of the file
-	 */
-	public String grepInvertFromOneFile(String pattern, String fileName);
-
-	/**
-	 * Returns string that doesn't contain lines which match the specified pattern 
-	 * in the given files
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param fileNames Array of file names
-	 */
-	public String grepInvertFromMultipleFiles(String pattern, String[] fileNames);
 }
