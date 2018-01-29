a140217d872bb794ce590d1f0de11b79982647b9
TK <teekayu8@hotmail.com>
2018-01-29 19:17:12 +0800

    Modify the spec file. Simplify and make Ls more elegant.

--- src/sg/edu/nus/comp/cs4218/app/Ls.java
+++ src/sg/edu/nus/comp/cs4218/app/Ls.java
@@ -5,63 +5,19 @@ import sg.edu.nus.comp.cs4218.Application;
 public interface Ls extends Application {
 	
 	/**
-	 * Returns string listing file information.
-	 * @param fileName String specifying name of the file
+	 * Return the string listing the folder content of 
+	 * the specified folders. If no folder names are 
+	 * specified, list the content of the current 
+	 * folder.
+	 * @param isFoldersOnly Boolean option to list only 
+	 * the folders
+	 * @param isRecursive Boolean option to recursively 
+	 * list the folder contents (traversing through all
+	 * folders inside the specified folder)
+	 * @param folderName Array of String of folder names
+	 * @throws Exception
 	 */
-	public String listFileInformation(String fileName);
-	
-	/**
-	 * Returns string listing multiple files information.
-	 * @param fileName String specifying name of the file
-	 */
-	public String listFilesInformation(String[] fileNames);
-	
-	/**
-	 * Returns string listing directories inside the current path
-	 * @param currentPath String specifying the current working directory
-	 */
-	public String listDirectoryFromCurrentPath(String currentPath);
-	
-	/**
-	 * Returns string listing directories recursively inside the current path
-	 * @param currentPath String specifying the current working directory
-	 */
-	public String listDirectoryRecursivelyFromCurrentPath(String currentPath);
-	
-	/**
-	 * Returns string listing directories and files inside the current path
-	 * @param currentPath String specifying the current working directory
-	 */
-	public String listAllFromCurrentPath(String currentPath);
-	
-	/**
-	 * Returns string listing directories and files recursively inside the current path
-	 * @param currentPath String specifying the current working directory
-	 */
-	public String listAllRecursivelyFromCurrentPath(String currentPath);
-	
-	/**
-	 * Returns string listing directories inside the given path
-	 * @param path String specifying a valid directory path
-	 */
-	public String listDirectoryFromPath(String path);
-	
-	/**
-	 * Returns string listing directories recursively inside the given path
-	 * @param path String specifying a valid directory path
-	 */
-	public String listDirectoryRecursivelyFromPath(String path);
-	
-	/**
-	 * Returns string listing directories and files inside the given path
-	 * @param path String specifying a valid directory path
-	 */
-	public String listAllFromPath(String path);
-	
-	/**
-	 * Returns string listing directories and files recursively inside the given path
-	 * @param path String specifying a valid directory path
-	 */
-	public String listAllRescursivelyFromPath(String path);
+	public String listFolderContent(Boolean isFoldersOnly, Boolean isRecursive, String... folderName) 
+			throws Exception;
 
 }
