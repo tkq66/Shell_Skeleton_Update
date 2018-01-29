81bcd99bf8277377fc78ed61a6432d7c183b3b9b
TK <teekayu8@hotmail.com>
2018-01-29 22:09:02 +0800

    Make Sed more elegant

--- src/sg/edu/nus/comp/cs4218/app/Sed.java
+++ src/sg/edu/nus/comp/cs4218/app/Sed.java
@@ -5,59 +5,41 @@ import java.io.InputStream;
 import sg.edu.nus.comp.cs4218.Application;
 
 public interface Sed extends Application {
-
-	/**
-	 * Returns string containing lines with the first matched substring (of that line)
-	 * replaced in file
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 * @param fileName String specifying name of the file
-	 */
-	public String replaceFirstSubStringInFile(String pattern, String replacement, String fileName);
 	
 	/**
-	 * Returns string containing lines with the xth matched substring (of that line)
-	 * replaced in file
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 * @param x Integer specifying the index of the substring to be replaced
+	 * Returns string of the file content with the matched 
+	 * substring on each line replaced. For each line, find
+	 * the substring that matched the pattern and replace 
+	 * the substring in the specified index of the matched 
+	 * substring list. 
+	 * @param pattern String specifying a regular expression 
+	 * in JAVA format
+	 * @param replacement String to replace the matched 
+	 * pattern
+	 * @param replacementIndex Integer specifying the index 
+	 * of the matched substring to be replaced
 	 * @param fileName String specifying name of the file
+	 * @throws Exception
 	 */
-	public String replaceXthSubStringInFile(String pattern, String replacement, int x, String fileName);
-
-	/**
-	 * Returns string containing lines with first matched substring (of that line)
-	 * replaced in Stdin
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 * @param stdin InputStream containing arguments from Stdin
-	 */
-	public String replaceFirstSubStringFromStdin(String pattern, String replacement, InputStream stdin);
+	public String replaceSubstringInFile(String pattern, String replacement, int replacementIndex, String fileName) 
+			throws Exception;
 	
 	/**
-	 * Returns string containing lines with the xth matched substring (of that line) 
-	 * replaced in Stdin
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 * @param x Integer specifying the index of the substring to be replaced
+	 * Returns string of the Stdin arg content with the matched 
+	 * substring on each line replaced. For each line, find
+	 * the substring that matched the pattern and replace 
+	 * the substring in the specified index of the matched 
+	 * substring list.
+	 * @param pattern String specifying a regular expression 
+	 * in JAVA format
+	 * @param replacement String to replace the matched 
+	 * pattern
+	 * @param replacementIndex Integer specifying the index 
+	 * of the matched substring to be replaced
 	 * @param stdin InputStream containing arguments from Stdin
+	 * @throws Exception
 	 */
-	public String replaceXthSubStringsFromStdin(String pattern, String replacement, int x, InputStream stdin);
-
-
-	/**
-	 * Returns string containing lines when invalid replacement string is
-	 * provided
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 */
-	public String replaceSubstringWithInvalidReplacement(String pattern, String replacement);
-
-	/**
-	 * Returns string containing lines when invalid regex is provided
-	 * @param pattern String specifying a regular expression in JAVA format
-	 * @param replacement String to replace the matched pattern
-	 */
-	public String replaceSubstringWithInvalidRegex(String pattern, String replacement);
+	public String replaceSubstringInStdin(String pattern, String replacement, int replacementIndex, InputStream stdin) 
+			throws Exception;
 
 }
